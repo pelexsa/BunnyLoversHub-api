98e9c20f624378c9e198f062e603ddc3b3ed8fb1
wwl.hmh <hmh@winwinlab.co.kr>
2024-02-15 10:43:32 +0900

    [add] first init

---
+++ src/main/java/com/view/bunnyview/BunnyViewApiApplication.java
@@ -0,0 +1,13 @@
+package com.view.bunnyview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnyViewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnyViewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/view/bunnyview/BunnyViewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.view.bunnyview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnyViewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
